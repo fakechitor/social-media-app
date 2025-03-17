c7b7c81014ffff4a542db5373edfbca62d383bfa
fakechitor <ladasedanbaklajan06@gmail.com>
2025-03-17 11:13:20 +0300

    feat: initialize authorization microservice

---
+++ social-media-authorization/src/main/kotlin/com/fakechitor/socialmediaauthorization/SocialMediaAuthorizationApplication.kt
@@ -0,0 +1,11 @@
+package com.fakechitor.socialmediaauthorization
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialMediaAuthorizationApplication
+
+fun main(args: Array<String>) {
+    runApplication<SocialMediaAuthorizationApplication>(*args)
+}
---
+++ social-media-authorization/src/test/kotlin/com/fakechitor/socialmediaauthorization/SocialMediaAuthorizationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fakechitor.socialmediaauthorization
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocialMediaAuthorizationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
