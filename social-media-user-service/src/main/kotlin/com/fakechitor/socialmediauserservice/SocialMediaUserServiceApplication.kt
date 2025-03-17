d2f589dc0871f282992a6b2af9935e85a15f8711
fakechitor <ladasedanbaklajan06@gmail.com>
2025-03-17 11:13:53 +0300

    feat: initialize user microservice

---
+++ social-media-user-service/src/main/kotlin/com/fakechitor/socialmediauserservice/SocialMediaUserServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.fakechitor.socialmediauserservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialMediaUserServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SocialMediaUserServiceApplication>(*args)
+}
---
+++ social-media-user-service/src/test/kotlin/com/fakechitor/socialmediauserservice/SocialMediaUserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fakechitor.socialmediauserservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocialMediaUserServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
