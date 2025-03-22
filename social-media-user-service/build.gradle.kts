1bcb0e2001d4fff3599e8a0c5464bdf02cff33c3
fakechitor <ladasedanbaklajan06@gmail.com>
2025-03-22 09:44:02 +0300

    build(gradle): add dependencies to user-service

--- social-media-user-service/build.gradle.kts
+++ social-media-user-service/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("kapt") version "1.9.25"
     id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jetbrains.kotlin.plugin.jpa") version "1.9.25"
@@ -24,6 +25,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.postgresql:postgresql:42.7.5")
+    implementation("org.mapstruct:mapstruct:1.6.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.3")
+    kapt("org.mapstruct:mapstruct-processor:1.6.3")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
