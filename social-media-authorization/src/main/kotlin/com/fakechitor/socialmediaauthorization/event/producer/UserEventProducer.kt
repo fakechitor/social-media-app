82a5e35fd3178b8e7049bd530463902c69c7682c
fakechitor <ladasedanbaklajan06@gmail.com>
2025-03-22 11:46:52 +0300

    feat(authorization-service): add kafka producer for user login/register events

---
+++ social-media-authorization/src/main/kotlin/com/fakechitor/socialmediaauthorization/event/producer/UserEventProducer.kt
@@ -0,0 +1,32 @@
+package com.fakechitor.socialmediaauthorization.event.producer
+
+import com.fakechitor.socialmediaauthorization.config.kafka.KafkaProducerConfig.Companion.USER_LOGIN_TOPIC
+import com.fakechitor.socialmediaauthorization.config.kafka.KafkaProducerConfig.Companion.USER_REGISTER_TOPIC
+import com.fakechitor.socialmediaauthorization.dto.UserLoginDto
+import com.fakechitor.socialmediaauthorization.dto.UserRegisterDto
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+
+@Component
+class UserEventProducer(
+    private val kafkaTemplate: KafkaTemplate<String, Any>,
+) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    fun sendRegister(userRegisterDto: UserRegisterDto) {
+        kafkaTemplate.send(
+            USER_REGISTER_TOPIC,
+            userRegisterDto,
+        )
+        logger.info("User registration message sent")
+    }
+
+    fun sendLogin(userLoginDto: UserLoginDto) {
+        kafkaTemplate.send(
+            USER_LOGIN_TOPIC,
+            userLoginDto,
+        )
+        logger.info("User login message sent")
+    }
+}
