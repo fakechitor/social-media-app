8ccd73a04cb8b94f6a97f0292fe00a3aae3c1f5d
fakechitor <ladasedanbaklajan06@gmail.com>
2025-03-25 19:37:37 +0300

    feat(user-service): add service methods for working with UserRepository

---
+++ social-media-user-service/src/main/kotlin/com/fakechitor/socialmediauserservice/exception/UserNotFoundException.kt
@@ -0,0 +1,11 @@
+package com.fakechitor.socialmediauserservice.exception
+
+class UserNotFoundException : RuntimeException {
+    constructor() : super()
+
+    constructor(message: String) : super(message)
+
+    constructor(message: String?, cause: Throwable?) : super(message, cause)
+
+    constructor(cause: Throwable?) : super(cause)
+}
--- social-media-user-service/src/main/kotlin/com/fakechitor/socialmediauserservice/service/UserService.kt
+++ social-media-user-service/src/main/kotlin/com/fakechitor/socialmediauserservice/service/UserService.kt
@@ -1,8 +1,11 @@
 package com.fakechitor.socialmediauserservice.service
 
 import com.fakechitor.socialmediauserservice.dto.mapper.UserMapper
+import com.fakechitor.socialmediauserservice.dto.response.UserResponseDto
+import com.fakechitor.socialmediauserservice.exception.UserNotFoundException
 import com.fakechitor.socialmediauserservice.model.User
 import com.fakechitor.socialmediauserservice.repository.UserRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -12,5 +15,11 @@ class UserService(
 ) {
     fun save(user: User) = userMapper.modelToDto(user = userRepository.save(user))
 
-    fun findByUsername(username: String) = userMapper.modelToDto(user = userRepository.findByUsername(username))
+    fun findByUsername(username: String) =
+        userRepository.findByUsername(username)?.let { userMapper.modelToDto(it) } ?: throw UserNotFoundException("User not found")
+
+    fun findById(id: Long) =
+        userRepository.findByIdOrNull(id)?.let { userMapper.modelToDto(it) } ?: throw UserNotFoundException("User not found")
+
+    fun findAll(): List<UserResponseDto> = userRepository.findAll().map { userMapper.modelToDto(it) }
 }
