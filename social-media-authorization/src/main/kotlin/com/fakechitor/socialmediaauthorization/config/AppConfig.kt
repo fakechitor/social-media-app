73eacee58372d3f72959a1c0e0d4a2bc076f001c
fakechitor <ladasedanbaklajan06@gmail.com>
2025-03-31 10:29:37 +0300

    feat(authorization): add webflux template for rest interaction

--- social-media-authorization/src/main/kotlin/com/fakechitor/socialmediaauthorization/config/AppConfig.kt
+++ social-media-authorization/src/main/kotlin/com/fakechitor/socialmediaauthorization/config/AppConfig.kt
@@ -2,8 +2,13 @@ package com.fakechitor.socialmediaauthorization.config
 
 import com.fakechitor.socialmediaauthorization.property.JwtProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
 
 @EnableConfigurationProperties(JwtProperties::class)
 @Configuration
-class AppConfig
+class AppConfig {
+    @Bean
+    fun webClient(): WebClient = WebClient.create()
+}
