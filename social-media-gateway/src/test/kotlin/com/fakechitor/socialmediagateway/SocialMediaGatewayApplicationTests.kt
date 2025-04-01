c41ca759a5cc38749c0870c147658525d843940e
fakechitor <ladasedanbaklajan06@gmail.com>
2025-04-01 14:47:48 +0300

    feat(gateway): add simple api gateway

---
+++ social-media-gateway/src/test/kotlin/com/fakechitor/socialmediagateway/SocialMediaGatewayApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.fakechitor.socialmediagateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocialMediaGatewayApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
