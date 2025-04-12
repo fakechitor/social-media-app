21b02771d3a8236ac3e8efebad1b3c07820e9160
fakechitor <ladasedanbaklajan06@gmail.com>
2025-04-12 12:09:54 +0300

    feat(post-service): initialize microservice for posts

---
+++ social-media-post-service/src/main/java/com/fakechitor/socialmediapostservice/SocialMediaPostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fakechitor.socialmediapostservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaPostServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaPostServiceApplication.class, args);
+    }
+
+}
---
+++ social-media-post-service/src/test/java/com/fakechitor/socialmediapostservice/SocialMediaPostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakechitor.socialmediapostservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaPostServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
