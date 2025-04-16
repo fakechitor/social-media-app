e0cf29cdc846ef4f1357c2152f15ef950df78561
fakechitor <ladasedanbaklajan06@gmail.com>
2025-04-16 13:58:04 +0300

    build(gradle): include other microservices to root settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
 rootProject.name = "social-media-app"
+include("social-media-gateway")
 include("social-media-authorization")
 include("social-media-user-service")
+include("social-media-post-service")
