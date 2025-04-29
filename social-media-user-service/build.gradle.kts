5e80bc1bfaae1fb8444f9c3e4c0cfa3b786875f1
fakechitor <ladasedanbaklajan06@gmail.com>
2025-04-29 13:24:07 +0300

    build(user-service): add dependency for validation

--- social-media-user-service/build.gradle.kts
+++ social-media-user-service/build.gradle.kts
@@ -24,6 +24,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.kafka:spring-kafka")
     implementation("org.postgresql:postgresql:42.7.5")
