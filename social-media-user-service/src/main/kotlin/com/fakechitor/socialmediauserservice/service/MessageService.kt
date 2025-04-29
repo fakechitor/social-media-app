ff7fdc13c48098a9fcb0ad21aa2406c29b285b58
fakechitor <ladasedanbaklajan06@gmail.com>
2025-04-29 13:34:30 +0300

    chore(user-service): remove unused variables

--- social-media-user-service/src/main/kotlin/com/fakechitor/socialmediauserservice/service/MessageService.kt
+++ social-media-user-service/src/main/kotlin/com/fakechitor/socialmediauserservice/service/MessageService.kt
@@ -3,8 +3,6 @@ package com.fakechitor.socialmediauserservice.service
 import com.fakechitor.socialmediauserservice.model.Message
 import com.fakechitor.socialmediauserservice.model.User
 import com.fakechitor.socialmediauserservice.repository.MessageRepository
-import jakarta.persistence.EntityManager
-import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.client.HttpServerErrorException
@@ -12,10 +10,7 @@ import org.springframework.web.client.HttpServerErrorException
 @Service
 class MessageService(
     private val messageRepository: MessageRepository,
-    private val entityManager: EntityManager,
 ) {
-    private val log = LoggerFactory.getLogger(javaClass)
-
     fun save(message: Message) =
         runCatching {
             messageRepository.save(message)
